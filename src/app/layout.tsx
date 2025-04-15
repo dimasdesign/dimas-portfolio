bc4501d2a07641a532db47c9831171763fc8ba8a
Eduardo P.P. Ferreira <ppfeduardo@gmail.com>
2025-04-15 08:22:28 -0300

    Adding font for whole site

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,11 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import localFont from 'next/font/local';
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const gothic = localFont({
+  src: './SpecialGothicExpandedOne-Regular.ttf',
+  display: 'swap',
+})
 
 export const metadata: Metadata = {
   title: "Dimas Design",
@@ -25,7 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${gothic.className} antialiased`}
       >
         {children}
       </body>
